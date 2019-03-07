a0d50f7041bc337da1f1a3cd6d0891a74149f046
Manu <mshalvagal@gmail.com>
2019-03-07 09:42:40 +0100

    Initial commit

---
+++ matlabFunctions/filterEOG.m
@@ -0,0 +1,20 @@
+function [b] = filterEOG( eeg, eog)
+    
+    % We perform regression following the paper:
+    % "[Sch�gl 2007] A fully automated correction method of EOG artifact in
+    % EEG recordings"
+    % Y = S + U*b
+    % Y: recorded EEG signal
+    % S: clean EEG signal   
+    % U: EOG channels
+    % b: EOG coefficients
+    
+    Y = eeg;
+    U = eog;
+    
+    covariance = cov([U Y]);
+    autoCovariance_EOG = covariance(1:size(U,2), 1:size(U,2));
+    crossCovariance_EOG_EEG = covariance(1:size(U,2), size(U,2)+1:end);
+    b = autoCovariance_EOG \ crossCovariance_EOG_EEG;    
+end
+
---
+++ src/data_loading.m
@@ -0,0 +1,31 @@
+[signal, header] = sload('ad4_Asynchronous_20192502152935.gdf');
+
+behavior = single(dlmread('ad4_Asynchronous_20192502152935.txt'));
+
+num_channels = 16;
+eeg = signal(:, 1:num_channels);
+eog = signal(:, 17:19);
+eggTrigger = signal(:, 33);
+behavioralTrigger = behavior(:, 2);
+
+eegIdx = find(diff(eggTrigger)) + 1;
+eegIdx = eegIdx(eggTrigger(eegIdx) == 3);
+behavIdx = find(diff(behavioralTrigger)) + 1;
+behavIdx = behavIdx(behavioralTrigger(behavIdx) == 3);
+magnitude = behavior(behavIdx, 3);
+
+corrTrialsIdx = find(magnitude==0);
+errTrialsIdx = find(magnitude>0);
+
+sampleTrial = -0.5*header.SampleRate:1.0*header.SampleRate;
+
+eegCorrTrials = zeros(length(sampleTrial), num_channels, length(corrTrialsIdx));
+eegErrTrials = zeros(length(sampleTrial), num_channels, length(errTrialsIdx));
+
+for idx = 1:length(corrTrialsIdx)
+    eegCorrTrials(:, :, idx) = signal(sampleTrial + eegIdx(corrTrialsIdx(idx)), 1:16); 
+end
+
+for idx = 1:length(errTrialsIdx)
+    eegErrTrials(:, :, idx) = signal(sampleTrial + eegIdx(errTrialsIdx(idx)), 1:16); 
+end
