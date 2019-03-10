226e8e961acc598d0774089e4102943f893d2d78
Oriane Peter <oriane.peter@epfl.ch>
2019-03-10 12:31:44 +0100

    created the baseline correction function

---
+++ src/baseline_correction.m
@@ -0,0 +1,16 @@
+function [ bl_output ] = baseline_correction( signal, window )
+% Compensates the offset so the EMG signal start at 0 
+%   This functions takes the first x values, x being defined by window, average them and define this value as the new 0
+size_matrix = size(signal);
+bl_output = zeros(size_matrix);
+for channel = 1:size_matrix(2)
+    for trial = 1:size_matrix(3) 
+        offset = mean(signal(1:window,1,1));
+        for tp = 1:size_matrix(1)
+            bl_output(tp,channel,trial) = signal(tp,channel,trial) - offset;
+        end
+    
+    end
+end
+end
+
