7d1087dfc867b45bc4a2bcb4fe6417a7be1413ba
Ayberk <ozkayberk@gmail.com>
2019-03-28 10:49:40 +0100

    PCA

---
+++ src/applyPCA.m
@@ -0,0 +1,7 @@
+function [trainProjected, testProjected] = applyPCA(trainData, testData, expVar)
+[Z,mu,sigma] = zscore(trainData);
+[coeff, score, ~, ~, explained] = pca(Z);
+index=find(cumsum(explained)>expVar);
+trainProjected = score(:,1:index(1));
+testProjected = (testData-mu)./sigma*coeff(:,1:index(1));
+end
