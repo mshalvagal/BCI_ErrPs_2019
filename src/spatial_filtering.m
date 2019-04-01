2b330430262556896a07dda2034cabdd473f940d
Apple <apple@tsf-452-wpa-2-078.epfl.ch>
2019-04-01 13:27:05 +0200

    added laplacian euclidean

--- src/spatial_filtering.m
+++ src/spatial_filtering.m
@@ -5,13 +5,19 @@ function filtered_signal = spatial_filtering(signal, type, varargin)
     % it will change the units of the data! I will check this later!
     if strcmp(type, 'CAR')
         filtered_signal = CAR_filter(signal);
-    elseif strcmp(type, 'Laplacian')
+    elseif strcmp(type, 'Laplacian perrin')
         % varargin should contain the chanlocs
         X = extractfield(varargin{1}, 'X');
         Y = extractfield(varargin{1}, 'Y');
         Z = extractfield(varargin{1}, 'Z');
         [surf_lap, ~, ~] = laplacian_perrinX(signal', X, Y, Z); 
         filtered_signal = surf_lap';
+    elseif strcmp(type, 'Laplacian euclidean')
+        % varargin should contain the chanlocs
+        X = extractfield(varargin{1}, 'X');
+        Y = extractfield(varargin{1}, 'Y');
+        Z = extractfield(varargin{1}, 'Z');
+        filtered_signal = laplacian_euclidean(signal, X, Y, Z);
     elseif strcmp(type, 'DAWN')
 
     elseif strcmp(type, 'CCN')
