a93de17a144946462b05a355432253ad10e37dbf
Oriane Peter <oriane.peter@epfl.ch>
2019-04-08 22:27:48 +0200

    CCA now return the filtering matrix

--- src/CCA.m
+++ src/CCA.m
@@ -1,12 +1,12 @@
-function [ CCA_corr, CCA_err ] = CCA(corr_trial, err_trial) 
+function [ W ] = CCA(corr_trial, err_trial) 
 % Using CCA to filter the signal   
 %  Input : epoch signal that already has been spectrally filtered for
 %           correct and erronous trials
 %          dim :T*M*K with T the time sample, M the channel and K the trial 
-%  Output : Filter signal. This signal has for each channel the maximum 
+%  Output : Filtering Matrix.When this matrix is applied the resulting signal has for each channel the maximum 
 %           correlation with the average over all trials of the same channel and is 
 %           uncorrelated to the average signal of the others channels (whitened)
-%           dim : same as input
+%           dim : M*M
 %  denaoted by X and the average over all trials  denoted by Y 
 % therefore correcting mostly for noise (which is not part of the average) 
 
@@ -21,7 +21,7 @@ size_concate = size(concate);
 Err_cat = reshape(concate,[],size_concate(2)*size_concate(3));
 X = [Corr_cat,Err_cat];
  
-%Create the average array
+%Create the average matrix 
 mean_corr = (mean_per_channel(corr_trial)).';
 mean_err = (mean_per_channel(err_trial)).';
  
@@ -30,15 +30,6 @@ Y = [repmat(mean_corr,1,size_concatc(3)),repmat(mean_err,1,size_concate(3))]; % 
 % performing the canonical correlation to get the spatial filter W
 [W,~] = canoncorr(X',Y');
  
-% Applying the spatial filter for every trial 
-CCA_corr = zeros(size(corr_trial, 1), size(W, 2), size(corr_trial, 3));
-for itrials = 1 : size_concatc(3)
-    CCA_corr(:,:,itrials) = corr_trial(:,:,itrials) * W;
-end
- 
-CCA_err = zeros(size(err_trial, 1), size(W, 2), size(err_trial, 3));
-for itrials = 1 : size_concate(3)
-    CCA_err(:,:,itrials) = err_trial(:,:,itrials) * W;
-end
+
  
 end
