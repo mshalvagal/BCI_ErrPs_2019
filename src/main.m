24c2df32ad5b6090a2072f10ffa42136f65876ad
Manu <mshalvagal@gmail.com>
2019-04-15 09:50:26 +0200

    Moved cross-validation loop back to main

--- src/main.m
+++ src/main.m
@@ -1,5 +1,23 @@
+%% Data loading
+
 [Trials, Labels, header] = preprocess_eeg('data/a7_20191103');
 % [X_test, y_test] = preprocess_eeg('data/b3_20191503', false);
 
-[confusion_matrices, percent_corrects] = model_assessment(Trials, Labels, header.SampleRate, 10, 'LDA', false, true, 64, 99);
+%% Model evaluation
+N = size(Trials, 3);
+num_folds = 5;
 
+cp = cvpartition(Labels, 'KFold', num_folds, 'Stratify', true);
+% cp = cvpartition(N, 'KFold', k);
+
+for i=1:num_folds
+    train_set = Trials(:, : ,cp.training(i));
+    test_set = Trials(:, :, cp.test(i));
+    train_labels = Labels(cp.training(i));
+    test_labels = Labels(cp.test(i));
+    
+    [confusion_matrix, percent_correct] = model_assessment(train_set, train_labels, test_set, test_labels, header.SampleRate, 'LDA', false, true, 64, 99);
+    
+    accuracies(i) = percent_correct;
+    confusion_matrices(:,:,i) = confusion_matrix;
+end
--- src/model_assessment.m
+++ src/model_assessment.m
@@ -1,42 +1,28 @@
-function [confusion_matrices, percent_corrects] = model_assessment(Trials, Labels, SR, k, model_type, do_CCA, do_PCA, downSR, expVarDesired)
+function [confusion_matrix, percent_correct] = model_assessment(train_set, train_labels, test_set, test_labels, SR, model_type, do_CCA, do_PCA, downSR, expVarDesired)
     % This functions gets the correct and error trials and performs 'k'-fold cross-validation 
     % on the 'model'
     
-    N = size(Trials, 3);
-    
-%     cp = cvpartition(Labels, 'KFold', k, 'Stratify', true);
-    cp = cvpartition(N, 'KFold', k);
-    
-    for i=1:k
-        train_set = Trials(:, : ,cp.training(i));
-        test_set = Trials(:, :, cp.test(i));
-        train_labels = Labels(cp.training(i));
-        test_labels = Labels(cp.test(i));
-        
-        if do_CCA
-            [train_set, test_set] = CCA(train_set, train_labels, test_set);
-        end
-        
-%         downSR = 64;
-%         expVarDesired = 95;
-        if do_PCA
-            [train_X, test_X] = feature_extraction(train_set, test_set, SR, downSR, expVarDesired);
-        else
-            temp = permute(train_set, [3,2,1]);
-            train_X = reshape(temp, size(temp, 1), size(temp, 2) * size(temp, 3));
-            temp = permute(test_set, [3,2,1]);
-            test_X = reshape(temp, size(temp, 1), size(temp, 2) * size(temp, 3));
-        end
-        
-        if strcmp(model_type, 'LDA')
-            Model = fitcdiscr(train_X, train_labels);
-            predictions = Model.predict(test_X);
-        end
-        
-        percent_corrects(i) = mean(predictions == test_labels);
-        confusion_matrices(:, :, i) = confusionmat(test_labels, predictions);
-        
+    if do_CCA
+        [train_set, test_set] = CCA(train_set, train_labels, test_set);
     end
-        
+
+%    downSR = 64;
+%    expVarDesired = 95;
+    if do_PCA
+        [train_X, test_X] = feature_extraction(train_set, test_set, SR, downSR, expVarDesired);
+    else
+        temp = permute(train_set, [3,2,1]);
+        train_X = reshape(temp, size(temp, 1), size(temp, 2) * size(temp, 3));
+        temp = permute(test_set, [3,2,1]);
+        test_X = reshape(temp, size(temp, 1), size(temp, 2) * size(temp, 3));
+    end
+
+    if strcmp(model_type, 'LDA')
+        Model = fitcdiscr(train_X, train_labels);
+        predictions = Model.predict(test_X);
+    end
+
+    percent_correct = mean(predictions == test_labels);
+    confusion_matrix = confusionmat(test_labels, predictions);        
         
 end
