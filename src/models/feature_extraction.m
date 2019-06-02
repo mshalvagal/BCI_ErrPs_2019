9a2a04618d056ac50fd2f60c5f8d1c2a365dfb54
Manu <mshalvagal@gmail.com>
2019-06-02 20:27:56 +0200

    Adding Ayberk's code

--- src/models/feature_extraction.m
+++ src/models/feature_extraction.m
@@ -1,17 +1,20 @@
 function [PCA, train_featuresProcessed, varargout] = feature_extraction(trainData, SR, downSampleRate, expVarDesired, varargin)
-    % [featuresProcessed] = extract_features(CorrTrials,header,downSampleRate,expVarDesired)
-    %
-    % This function takes epoch data as input
-    % and takes t = [200,800] ms of each trial and downsamples them in 64Hz, then
-    % concatenated to form a vector of features. The feature vectors of all
-    % trials were normalized, and then decorrelated using PCA, retaining 95% of the explained variance.
+% [featuresProcessed] = extract_features(CorrTrials,header,downSampleRate,expVarDesired)
+%
+% This function takes epoch data as input
+% and takes t = [200,800] ms of each trial and downsamples them in 64Hz, then
+% concatenated to form a vector of features. The feature vectors of all
+% trials were normalized, and then decorrelated using PCA, retaining 95% of the explained variance.
 
-    %% Feature extraction
-    % featuresExtracted=zeros(312,length(eegIdx));
-    % eight fronto-central channels (Fz, FC1, FCz, FC2, C1, Cz, C2, and CPz)
-    selected_channels = [1 3 4 5 8 9 10 14];
-    
-    train_featuresExtracted = [];
+%% Feature extraction
+% featuresExtracted=zeros(312,length(eegIdx));
+% eight fronto-central channels (Fz, FC1, FCz, FC2, C1, Cz, C2, and CPz)
+selected_channels = [1 3 4 5 8 9 10 14];
+
+train_featuresExtracted = [];
+PCA =[]; 
+
+if expVarDesired ~= -1
     for idx=1:size(trainData, 3)
         temp = trainData(floor(0.7*SR:1.3*SR+1), selected_channels, idx); % selected 8 channels
         hz64 = downsample(temp, SR / downSampleRate); %512/64
@@ -21,7 +24,6 @@ function [PCA, train_featuresProcessed, varargout] = feature_extraction(trainDat
         end
         train_featuresExtracted(:,idx) = featureVector;
     end
-    
     if nargin==5
         testData = varargin{1};
         test_featuresExtracted = [];
@@ -34,12 +36,42 @@ function [PCA, train_featuresProcessed, varargout] = feature_extraction(trainDat
             end
             test_featuresExtracted(:,idx) = featureVector;
         end
-
+        
         [PCA, train_featuresProcessed, test_featuresProcessed] = applyPCA(train_featuresExtracted', expVarDesired, test_featuresExtracted');
         varargout{1} = test_featuresProcessed;
     else
-        [PCA, train_featuresProcessed] = applyPCA(train_featuresExtracted', expVarDesired); 
+        [PCA, train_featuresProcessed] = applyPCA(train_featuresExtracted', expVarDesired);
     end
-    
-
+else
+    for idx=1:size(trainData, 3)
+        temp = trainData(floor(0.7*SR:1.3*SR+1), selected_channels, idx); % selected 8 channels
+        hz64 = downsample(temp, SR / downSampleRate); %512/64
+        featureVector = [];
+        for j=1:length(selected_channels)
+            featureVector = [featureVector; hz64(:,j)];
+        end
+        train_featuresExtracted(:,idx) = featureVector;
+    end
+    if nargin==5
+        testData = varargin{1};
+        test_featuresExtracted = [];
+        for idx=1:size(testData, 3)
+            temp = testData(floor(0.7*SR:1.3*SR+1), selected_channels, idx); % selected 8 channels
+            hz64 = downsample(temp, SR / downSampleRate); %512/64
+            featureVector = [];
+            for j=1:length(selected_channels)
+                featureVector = [featureVector; hz64(:,j)];
+            end
+            test_featuresExtracted(:,idx) = featureVector;
+        end
+        
+        %[PCA, train_featuresProcessed, test_featuresProcessed] = applyPCA(train_featuresExtracted', expVarDesired, test_featuresExtracted');
+        varargout{1} = test_featuresExtracted';
+        train_featuresProcessed = train_featuresExtracted';
+    else
+        %[PCA, train_featuresProcessed] = applyPCA(train_featuresExtracted', expVarDesired);
+        train_featuresProcessed = train_featuresExtracted';
+        
+    end
+end
 end
